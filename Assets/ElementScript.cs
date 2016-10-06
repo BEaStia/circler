db4ddd9394a607f7bb1d216d51c470039ce1e68c
igor <gophan1992@gmail.com>
2016-10-06 07:18:26 +0300

    initial

---
+++ Assets/ElementScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using DigitalRuby.Tween;
+using UnityEngine.UI;
+
+public class ElementScript : MonoBehaviour
+{
+
+    public Element Element;
+    public Color Color;
+    public int Id;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    GetComponent<Image>().color = Color;
+        var child = transform.GetChild(0);
+	    var text = child.gameObject.GetComponent<Text>();
+        text.text = Element.Name;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Move(Vector3 newPos)
+    {
+        Debug.Log("started at "+Id.ToString());
+        TweenFactory.Tween("MoveCircle"+ Id, transform.localPosition, newPos, 1.75f, TweenScaleFunctions.CubicEaseIn, (t) =>
+        {
+            transform.localPosition = t.CurrentValue;
+        }, (t3) =>
+        {
+            Debug.Log("Finished at " + Id.ToString());
+            // completion - nothing more to do!
+        });
+    }
+}
---
+++ Assets/GameControllerScript.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using DigitalRuby.Tween;
+using UnityEngine.EventSystems;
+
+
+public class Element
+{
+    public GameControllerScript.ElementTypes ElementType;
+    public string Name;
+    public GameObject Object;
+}
+
+public class GameControllerScript : MonoBehaviour
+{
+
+    public enum ElementTypes
+    {
+        Element,
+        Plus
+    }
+
+    public int Count = 2;
+    public float Angle, Radius = 450f;
+    public List<Element> Elements = new List<Element>();
+
+    // Use this for initialization
+
+    public void Start()
+    {
+        Angle = Mathf.PI*2/Count;
+        for (var i = 0; i < Count; i++)
+        {
+            Elements.Add(new Element {ElementType = ElementTypes.Element, Name = "He"});
+        }
+        PlaceElements();
+    }
+
+    public void PlaceElements()
+    {
+        Angle = Mathf.PI*2/Elements.Count;
+        var prefab = Resources.Load("circlePrefab");
+        var parent = GameObject.Find("Canvas").transform.Find("Field").gameObject;
+        /*for (var i = 0; i < parent.transform.childCount; i++)
+        {
+            Destroy(parent.transform.GetChild(i).gameObject);
+        }*/
+
+        var scale = ((RectTransform) parent.transform).sizeDelta/2;
+        for (var i = 0; i < Elements.Count; i++)
+        {
+            if (Elements[i].Object == null)
+            {
+                var gO = Instantiate(prefab) as GameObject;
+                if (gO == null) continue;
+                gO.GetComponent<ElementScript>().Element = Elements[i];
+                gO.transform.SetParent(parent.transform);
+                gO.transform.localPosition = new Vector3(Radius*Mathf.Cos(Angle*i) + scale.x,
+                    Radius*Mathf.Sin(Angle*i) + scale.y, 0);
+                gO.GetComponent<ElementScript>().Id = i;
+                Elements[i].Object = gO;
+            }
+            else
+            {
+                var newPos = new Vector3(Radius*Mathf.Cos(Angle*i) + scale.x,
+                    Radius*Mathf.Sin(Angle*i) + scale.y, 0);
+                Elements[i].Object.GetComponent<ElementScript>().Id = i;
+                Elements[i].Object.GetComponent<ElementScript>().Move(newPos);
+            }
+        }
+    }
+
+    public void AddPlus()
+    {
+        var prefab = Resources.Load("circlePrefab");
+        var parent = GameObject.Find("Canvas").transform.Find("Field").gameObject;
+        var gO = Instantiate(prefab) as GameObject;
+        if (gO != null)
+        {
+            gO.transform.SetParent(parent.transform);
+            gO.GetComponent<ElementScript>().Element = new Element {ElementType = ElementTypes.Element, Name = "He"};
+            gO.transform.localPosition = new Vector3(0, 0, 0);
+        }
+
+        Elements.Add(new Element {ElementType = ElementTypes.Element, Name = "He", Object = gO});
+        PlaceElements();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClick(BaseEventData eventData)
+    {
+        Debug.Log("Click!");
+        var pointerEventData = (PointerEventData) eventData;
+        var center = new Vector2(x: Screen.width/2.0f, y: Screen.height/2.0f);
+        var position = pointerEventData.position - center;
+        var angle = Mathf.Atan2(position.y, position.x);
+        if (angle < 0f)
+            angle = Mathf.PI*2 + angle;
+        for (var i = 0; i < Elements.Count; i++)
+        {
+            if (!(i*Angle < angle) || !((i + 1)*Angle > angle)) continue;
+            var prefab = Resources.Load("circlePrefab");
+            var parent = GameObject.Find("Canvas").transform.Find("Field").gameObject;
+            var gO = Instantiate(prefab) as GameObject;
+            if (gO == null) continue;
+            gO.transform.SetParent(parent.transform);
+            gO.GetComponent<ElementScript>().Element = new Element {ElementType = ElementTypes.Element, Name = "He"};
+            gO.transform.localPosition = position;
+            Elements.Insert(Elements[i].Object.GetComponent<ElementScript>().Id + 1,
+                new Element {ElementType = ElementTypes.Element, Name = "He", Object = gO});
+        }
+        PlaceElements();
+    }
+}
